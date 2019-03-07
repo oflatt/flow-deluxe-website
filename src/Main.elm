40007dc2655a22b0dca1e4c7d625ecd0291ebd6c
oliver <oflatt@gmail.com>
2019-03-07 10:15:54 -0700

    start css styling and update model

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,38 @@
+module Main exposing (main)
+
 import Browser
-import Html exposing (Html, button, div, text, node)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Css exposing (..)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, href, src, rel)
+import Html.Styled.Events exposing (onClick)
 
 
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
-
+    Browser.sandbox
+        { view = view >> toUnstyled
+        , update = update
+        , init = initialModel
+        }
 
 -- MODEL
 
-type alias Model = Int
+type alias PageName = String
 
-init : Model
-init =
-  0
+type alias Model = {currentPage: PageName}
+
+initialModel : Model
+initialModel = Model "home"
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = ChangePage PageName
 
 update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
+update msg model = case msg of
+                       ChangePage pageName -> Model pageName
 
 -- VIEW
 
@@ -41,24 +44,64 @@ view model =
                     ,href "https://fonts.googleapis.com/css?family=Tangerine"][]
       , node "link" [rel "stylesheet"
                     ,href "https://fonts.googleapis.com/css?family=Raleway"][]
-      ,div[ style "background-color" "rgb(165, 220, 255)"
-          , style "height" "90px"
-          , style "width" "100%"
+
+       -- title
+      ,div[css [
+            backgroundColor (rgb 165 220 255)
+           ,height (px 90)
+           ,width (pct 100)
+           ]
           ]
-            [div[style "text-align" "center"]
-                 [div[style "font-family" "Raleway,Light"
-                     ,style "font-size" "32px"
-                     ,style "font-style" "Light"
-                     ,style "margin-top" "10px"
-                     ,style "display" "inline-block"]
+            [div[css[
+                  textAlign (center)
+                 ,border (px 10)
+                 ]
+                ]
+                 [div[css[
+                       fontFamilies ["Raleway"]
+                      ,fontSize (px 32)
+                      ,top (px 10)
+                      ,display (inlineBlock)
+                      ]]
                       [text "FLOW"]
-                 ,div[style "font-family" "Tangerine,serif"
-                     ,style "font-size" "40px"
-                     ,style "font-style" "italic"
-                     ,style "color" "rgb(247, 54, 105)"
-                     ,style "display" "inline-block"]
-                 [text "Deluxe"]]]
-      , button [ onClick Decrement ] [ text "-" ]
-      , div [] [ text (String.fromInt model) ]
-      , button [ onClick Increment ] [ text "+" ]
+                 ,div[css[fontFamilies ["Tangerine, serif"]
+                         ,fontSize (px 40)
+                         ,fontStyle italic
+                         ,color (rgb 247 54 105)
+                         ,display inlineBlock
+                         ]
+                     ]
+                      [text "Deluxe"]]]
+
+      -- page buttons
+      , (pagebutton "home")
+                
+      -- home page
+      , (listing "../assets/placeholder.png"
+             "Listing title" model)
+
+      -- listing page
       ]
+
+
+pagebutton pageName = button [onClick (ChangePage pageName)
+                             ,css
+                             [width (pct 20)
+                             ,height (px 50)
+                             ]]
+                      [text pageName]
+
+      
+pagedisplay pagename model = if model.currentPage == pagename then display block else display none
+
+                             
+
+listing imgName title model =
+    div [css[(pagedisplay "listings" model)]]
+        [img [src imgName
+             , css [
+                   left (pct 10)
+                  ,top (px 10)
+                  ,height (pct 50)
+                  ]][]
+         ,text title]
