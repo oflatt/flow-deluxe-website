fce56b568a252d53bbdc5675cc086e81658d99ce
oliver <oflatt@gmail.com>
2019-10-04 18:21:40 -0600

    start moving into modules

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 module Main exposing (main)
+import Model
 
 import Task
 import Browser
@@ -31,42 +32,6 @@ main =
         , onUrlRequest = LinkClicked
         }
 
-
-        
--- MODEL
-
-type alias PageName = String
-type alias MousePos = (Float, Float)
-
-type alias Model = {currentPage: PageName
-                   ,highlightedButton: PageName
-                   ,urlkey : Nav.Key
-                   ,url : Url.Url
-                   ,indexurl : String
-                   ,windowWidth : Int
-                   ,windowHeight : Int}
-
-getindexurl url =
-    let str = (Url.toString url)
-    in
-    (String.slice 0 ((String.length str)-(String.length url.path)) str)
-
-
-type alias Flags = {innerWindowWidth : Int,
-                   innerWindowHeight : Int,
-                   outerWindowWidth : Int,
-                   outerWindowHeight : Int}
-
-
-    
-initialModel : Flags -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
-initialModel flags url key = ((Model
-                                   (urlToPageName url)
-                                   "none"
-                                   key url
-                                   (getindexurl url)
-                                   flags.innerWindowWidth
-                                   flags.innerWindowHeight), Cmd.none)
 
 
 -- SUBSCRIPTIONS
---
+++ src/Model.elm
@@ -0,0 +1,34 @@
+module Model exposing (PageName, MousePos, Model, getindexurl, initialModel)
+
+type alias PageName = String
+type alias MousePos = (Float, Float)
+
+type alias Model = {currentPage: PageName
+                   ,highlightedButton: PageName
+                   ,urlkey : Nav.Key
+                   ,url : Url.Url
+                   ,indexurl : String
+                   ,windowWidth : Int
+                   ,windowHeight : Int}
+
+getindexurl url =
+    let str = (Url.toString url)
+    in
+    (String.slice 0 ((String.length str)-(String.length url.path)) str)
+
+
+type alias Flags = {innerWindowWidth : Int,
+                   innerWindowHeight : Int,
+                   outerWindowWidth : Int,
+                   outerWindowHeight : Int}
+
+
+    
+initialModel : Flags -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+initialModel flags url key = ((Model
+                                   (urlToPageName url)
+                                   "none"
+                                   key url
+                                   (getindexurl url)
+                                   flags.innerWindowWidth
+                                   flags.innerWindowHeight), Cmd.none)
